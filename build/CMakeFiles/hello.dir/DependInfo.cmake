d65126c1e05618ad8267e8c64ccefe3c3053b3db
mvshalamov <mvshalamov@gmail.com>
2020-10-07 14:21:03 +0300

    first version

---
+++ build/CMakeFiles/hello.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/shalamovs/Desktop/robots/first_step/script/hello.cpp" "/home/shalamovs/Desktop/robots/first_step/build/CMakeFiles/hello.dir/script/hello.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_TEST_DYN_LINK"
+  "LIBBULLET_VERSION=2.87"
+  "LIBBULLET_VERSION_GT_282"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/gazebo-11"
+  "/usr/include/bullet"
+  "/usr/include/simbody"
+  "/usr/include/sdformat-9.3"
+  "/usr/include/ignition/math6"
+  "/usr/include/OGRE"
+  "/usr/include/OGRE/Terrain"
+  "/usr/include/OGRE/Paging"
+  "/usr/include/ignition/transport8"
+  "/usr/include/ignition/msgs5"
+  "/usr/include/ignition/common3"
+  "/usr/include/ignition/fuel_tools4"
+  "/usr/include/eigen3"
+  "/usr/include/sdformat-9.3/sdf/.."
+  "/usr/include/ignition/cmake2"
+  "/usr/include/uuid"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
